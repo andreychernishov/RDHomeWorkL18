14279d53980b361e9561215196eb3a22a1909362
Andrii Chernyshov <andruha46@gmail.com>
2023-09-24 21:29:59 +0300

    ModelViewPresenter starts Its Works

--- app/src/main/java/com/example/rdhomeworkl18/MainActivity.kt
+++ app/src/main/java/com/example/rdhomeworkl18/MainActivity.kt
@@ -4,16 +4,34 @@ import android.app.Activity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.example.rdhomeworkl18.ModelViewPresenter.MyView
+import com.example.rdhomeworkl18.ModelViewPresenter.Presenter
+import com.example.rdhomeworkl18.ModelViewPresenter.PresenterImpl
 
+var mainTv: TextView? = null
+val presenter: Presenter = PresenterImpl()
 
-class MainActivity: Activity() {
+class MainActivity: Activity(), MyView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity_layout)
+        mainTv = findViewById(R.id.main_tv)
 
-        val mainTv: TextView = findViewById(R.id.main_tv)
         val btnPlus: Button = findViewById(R.id.plus_btn)
         val btnMinus: Button = findViewById(R.id.minus_btn)
+        presenter.setView(this)
+        btnPlus.setOnClickListener {
+            presenter.plus()
+        }
+        btnMinus.setOnClickListener {
+            presenter.minus()
+
+        }
+
+    }
+
+    override fun updateCounter(counter: Int) {
+        mainTv?.text = counter.toString()
     }
 }
---
+++ app/src/main/java/com/example/rdhomeworkl18/ModelViewPresenter/ModelInterface.kt
@@ -0,0 +1,7 @@
+package com.example.rdhomeworkl18.ModelViewPresenter
+
+interface ModelInterface {
+    fun getCounter(): Int
+    fun plus()
+    fun minus()
+}
